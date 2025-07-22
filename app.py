270a73f687c5e68c39743520fce042883c5a1142
Yogi <yogeshwaransiva65@gmail.com>
2025-07-22 16:35:30 +0530

    initial commit

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
-from bottle import Bottle
-from bottle import SimpleTemplate
-from bottle import run
-import os
+from bottle import Bottle, run, template, TEMPLATE_PATH
+
+# Tell Bottle to look in the "templates" folder for .tpl files
+TEMPLATE_PATH.insert(0, 'templates')
 
 app = Bottle()
 
 @app.route('/')
 def home_page():
-    template = os.path.join(os.path.dirname(__file__), 'templates', 'index.tpl')
-    content = open(template).read()
-    tpl = SimpleTemplate(source=content)
-    return tpl.render()
+    return template('index')  # This will render templates/index.tpl
 
 run(app, host='localhost', port=9090)
+
